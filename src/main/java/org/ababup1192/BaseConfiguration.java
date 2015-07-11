a5aac6badafd15fd185a0afb6deddfb55e004117
ababup1192 <ababup1192@gmail.com>
2015-07-11 17:35:22 +0900

    First commit

---
+++ src/main/java/org/ababup1192/BaseConfiguration.java
@@ -0,0 +1,11 @@
+package org.ababup1192;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseConfiguration.class, args);
+    }
+}
